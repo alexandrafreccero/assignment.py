89796258089626129a88dcbcaa1abd1aae2f69eb
Alexandra <alexandrafreccero@gmail.com>
2020-11-27 10:34:50 +0100

    added plan on how to solve assignmnet

---
+++ calculator.py
@@ -0,0 +1,18 @@
+'''
+start try catch block before user input. 
+input 1 and 2 from user. num 1 num 2 user input -get two numbers from user
+input 3 from user= let user choose +*-/
+create function/method that iterates list;[] 0-50,50-100
+# create if elif or switch +/*-
+# answer in first loop, iterates for loop list [] 0-50,50-100, 100and decides answer
+answer leads to print line:
+1<50 printa ”Less then fifty”
+50 printa ”Fifty”
+mer än 50 men mindre än 100 ”Almost a hundred”
+100 printa ut ”Spot on!”
+mer än 100 printa ”Missed the spot!”
+add elif: retry
+end with catch exception  "Something went wrong, retry"'''
+
+
+
